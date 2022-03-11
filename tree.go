675ef08bc3e906e956bd28a1be0a223d62b0f276
Nathanael Walker-Hale <nathanael.walker.hale@gmail.com>
2022-03-11 09:29:40 +0000

    starter for trees

---
+++ tree.go
@@ -0,0 +1,29 @@
+package gochujang
+
+// borrowing heavily from Stephen Smith, but this is just for practice
+
+type Node struct {
+	label    string
+	length   float64
+	parent   *Node
+	children []*Node
+	data     map[string]int
+	istip    bool
+	note     string
+	number   int
+}
+
+func NewNode() *Node {
+	return &Node{}
+}
+
+func parse_nwk(nwk string) (root *Node) {
+	rt := Node{label: "", length: 0.0, parent: nil}
+	for i, v := range nwk {
+		if v == "(" {
+			
+		}
+	}
+}
+
+((A,B),C,D);
